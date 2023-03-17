5e38fab7ab80ffd16243afbb4cd1bad73d7e00e5
Paweł Iwaneczko <p.iwaneczko@endego.com>
2023-03-17 12:56:19 +0100

    Setting alpha buffer values

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,10 +2,14 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlComponent>
+#include <QQuickWindow>
+#include <QSurfaceFormat>
 
 int main(int argc, char* argv[]) {
     QApplication::setQuitOnLastWindowClosed(false);
     QApplication app(argc, argv);
+    QQuickWindow::setDefaultAlphaBuffer(true);
+    QSurfaceFormat::defaultFormat().setAlphaBufferSize(1);
     QQmlEngine engine;
     QQmlComponent(&engine, "qml/main.qml").create();
 
